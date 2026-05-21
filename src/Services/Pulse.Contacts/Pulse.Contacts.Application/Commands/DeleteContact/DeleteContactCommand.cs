2b0c53725966a92897459aa2847968f67b810b38
Dylan-JM <dylanjmarley01@gmail.com>
2026-05-21 20:18:08 +0100

    feat: Add DeleteContact command and handler
    Add DeleteContactCommand and DeleteContactHandler using MediatR.
    Handler removes a Contact by ID via IContactRepository.

---
+++ src/Services/Pulse.Contacts/Pulse.Contacts.Application/Commands/DeleteContact/DeleteContactCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Pulse.Contacts.Application.Commands.DeleteContact;
+
+public class DeleteContactCommand : IRequest
+{
+    public Guid ContactId { get; }
+
+    public DeleteContactCommand(Guid contactId)
+    {
+        ContactId = contactId;
+    }
+}
---
+++ src/Services/Pulse.Contacts/Pulse.Contacts.Application/Commands/DeleteContact/DeleteContactHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Pulse.Contacts.Domain.Repositories;
+
+namespace Pulse.Contacts.Application.Commands.DeleteContact;
+
+public class DeleteContactHandler : IRequestHandler<DeleteContactCommand>
+{
+    private readonly IContactRepository _contactRepository;
+
+    public DeleteContactHandler(IContactRepository contactRepository)
+    {
+        _contactRepository = contactRepository;
+    }
+
+    public async Task Handle(DeleteContactCommand command, CancellationToken cancellationToken)
+    {
+        await _contactRepository.Remove(command.ContactId);
+    }
+}
