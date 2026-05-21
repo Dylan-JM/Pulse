851c01dfea38f3a5074e15ee53d6f2e91cad0dec
Dylan-JM <dylanjmarley01@gmail.com>
2026-05-21 22:19:42 +0100

    feat: Add ContactsDbContext
    Add EF Core DbContext with Contacts and ContactLists DbSets in the Infrastructure persistence layer.

---
+++ src/Services/Pulse.Contacts/Pulse.Contacts.Infrastructure/Persistence/ContactsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pulse.Contacts.Domain.Entities;
+
+namespace Pulse.Contacts.Infrastructure.Persistence;
+
+public class ContactsDbContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<ContactList> ContactLists { get; set; }
+
+    public ContactsDbContext(DbContextOptions<ContactsDbContext> options)
+        : base(options) { }
+}
