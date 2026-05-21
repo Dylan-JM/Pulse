7ad896686626e257f996cf4364368947a147065a
Dylan-JM <dylanjmarley01@gmail.com>
2026-05-21 19:33:35 +0100

    feat: Add repository interfaces to Domain layer
    Add IContactRepository and IContactListRepository interfaces
    defining data access contracts for Contact and ContactList aggregates.

---
+++ src/Services/Pulse.Contacts/Pulse.Contacts.Domain/Repositories/IContactListRepository.cs
@@ -0,0 +1,10 @@
+﻿using Pulse.Contacts.Domain.Entities;
+
+namespace Pulse.Contacts.Domain.Repositories;
+
+public interface IContactListRepository
+{
+    Task<ContactList?> GetById(Guid id);
+    Task Add(ContactList contactList);
+    Task Remove(Guid id);
+}
---
+++ src/Services/Pulse.Contacts/Pulse.Contacts.Domain/Repositories/IContactRepository.cs
@@ -0,0 +1,11 @@
+﻿using Pulse.Contacts.Domain.Entities;
+
+namespace Pulse.Contacts.Domain.Repositories;
+
+public interface IContactRepository
+{
+    Task<Contact?> GetById(Guid id);
+    Task<List<Contact>> GetAll();
+    Task Add(Contact contact);
+    Task Remove(Guid id);
+}
