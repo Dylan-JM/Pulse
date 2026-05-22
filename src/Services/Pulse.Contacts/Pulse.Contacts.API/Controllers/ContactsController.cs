f7a48939691d27042f449c4e058befcdb78b77a6
Dylan-JM <dylanjmarley01@gmail.com>
2026-05-22 15:56:26 +0100

    feat: add ContactsController with CRUD endpoints
    GET all, GET by id, POST create, and DELETE endpoints
    wired up via MediatR ISender.

---
+++ src/Services/Pulse.Contacts/Pulse.Contacts.API/Controllers/ContactsController.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Pulse.Contacts.Application.Commands.CreateContact;
+using Pulse.Contacts.Application.Commands.DeleteContact;
+using Pulse.Contacts.Application.Queries.GetAllContacts;
+using Pulse.Contacts.Application.Queries.GetContactById;
+using Pulse.Contacts.Domain.Entities;
+
+namespace Pulse.Contacts.API.Controllers;
+
+[ApiController]
+[Route("api/[contacts]")]
+public class ContactsController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public ContactsController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet]
+    public async Task<List<Contact>> GetAll()
+    {
+        var contactQuery = new GetAllContactsQuery();
+        return await _sender.Send(contactQuery);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<Contact> GetById(Guid id)
+    {
+        var contactQuery = new GetContactByIdQuery(id);
+        return await _sender.Send(contactQuery);
+    }
+
+    [HttpPost]
+    public async Task CreateContact([FromBody] CreateContactCommand command)
+    {
+        await _sender.Send(command);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task DeleteContact(Guid id)
+    {
+        var deleteQuery = new DeleteContactCommand(id);
+        await _sender.Send(deleteQuery);
+    }
+}
