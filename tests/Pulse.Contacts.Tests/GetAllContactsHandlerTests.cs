2d691932b43b0720f72fa83242fc88f1ae377795
Dylan-JM <dylanjmarley01@gmail.com>
2026-06-12 14:12:30 +0100

    test: Unit tests for Contact query handlers
    GetAllContacts returns correct list, GetContactById returns correct contact.

---
+++ tests/Pulse.Contacts.Tests/GetAllContactsHandlerTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Moq;
+using Pulse.Contacts.Application.Queries.GetAllContacts;
+using Pulse.Contacts.Domain.Entities;
+using Pulse.Contacts.Domain.Repositories;
+using Pulse.Contacts.Domain.ValueObjects;
+using Xunit;
+
+namespace Pulse.Contacts.Tests;
+
+public class GetAllContactsHandlerTests
+{
+    [Fact]
+    public async Task Handle_ReturnsAllContactsFromRepository()
+    {
+        // Arrange
+        var mockRepo = new Mock<IContactRepository>();
+        var contacts = new List<Contact>
+        {
+            new Contact("Test User", Email.Create("test@email.com"), "0777777"),
+        };
+        mockRepo.Setup(r => r.GetAll()).ReturnsAsync(contacts);
+        var query = new GetAllContactsQuery();
+        var handler = new GetAllContactsHandler(mockRepo.Object);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().HaveCount(1);
+        result[0].Name.Should().Be("Test User");
+    }
+}
---
+++ tests/Pulse.Contacts.Tests/GetContactByIDHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Moq;
+using Pulse.Contacts.Application.Queries.GetAllContacts;
+using Pulse.Contacts.Application.Queries.GetContactById;
+using Pulse.Contacts.Domain.Entities;
+using Pulse.Contacts.Domain.Repositories;
+using Pulse.Contacts.Domain.ValueObjects;
+using Xunit;
+
+namespace Pulse.Contacts.Tests;
+
+public class GetContactByIDHandlerTests
+{
+    [Fact]
+    public async Task Handle_ReturnContactByIDFromRepository()
+    {
+        // Arrange
+        var mockRepo = new Mock<IContactRepository>();
+        var contact = new Contact("Test User", Email.Create("test@email.com"), "0777777");
+        var id = Guid.NewGuid();
+        mockRepo.Setup(r => r.GetById(id)).ReturnsAsync(contact);
+        var query = new GetContactByIdQuery(id);
+        var handler = new GetContactByIdHandler(mockRepo.Object);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Name.Should().Be("Test User");
+    }
+}
