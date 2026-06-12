f9a1ee0728168bd6648377d11d29bc868ebbeb08
Dylan-JM <dylanjmarley01@gmail.com>
2026-06-12 14:02:11 +0100

    tests: unit tests

---
+++ tests/Pulse.Contacts.Tests/CreateContactHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using Pulse.Contacts.Application.Commands.CreateContact;
+using Pulse.Contacts.Domain.Entities;
+using Pulse.Contacts.Domain.Repositories;
+using Xunit;
+
+namespace Pulse.Contacts.Tests;
+
+public class CreateContactHandlerTests
+{
+    [Fact]
+    public async Task Handle_WithValidCommand_AddsContactToRepository()
+    {
+        var mockRepo = new Mock<IContactRepository>();
+        var command = new CreateContactCommand("Test User", "test@email.com", "0777777");
+        var handler = new CreateContactHandler(mockRepo.Object);
+
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        mockRepo.Verify(r => r.Add(It.IsAny<Contact>()), Times.Once);
+    }
+}
---
+++ tests/Pulse.Contacts.Tests/DeleteContactHandlerTests.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using Pulse.Contacts.Application.Commands.DeleteContact;
+using Pulse.Contacts.Domain.Repositories;
+using Xunit;
+
+namespace Pulse.Contacts.Tests;
+
+public class DeleteContactHandlerTests
+{
+    [Fact]
+    public async Task Handle_WithValidCommand_DeleteContactFromRepository()
+    {
+        var mockRepo = new Mock<IContactRepository>();
+        var command = new DeleteContactCommand(Guid.NewGuid());
+        var handler = new DeleteContactHandler(mockRepo.Object);
+
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        mockRepo.Verify(r => r.Remove(command.ContactId), Times.Once);
+    }
+}
